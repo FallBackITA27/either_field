9b71a47efa84588c441aa0873cad9424d3d158b2
FallBackITA27 <diegomassanova4@gmail.com>
2025-03-16 12:04:14 +0100

    proof of concept generics

--- either_field_macros/src/lib.rs
+++ either_field_macros/src/lib.rs
@@ -1,6 +1,22 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::ToTokens;
-use syn::{Data, Token, Type, parse::Parse, parse_macro_input};
+use syn::{
+    Data, GenericParam, Ident, Token, Type, parse::Parse, parse_macro_input, punctuated::Punctuated,
+};
+
+fn get_alpha(n: usize) -> String {
+    const ALPHABET: [char; 26] = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+    if n < 26 {
+        return ALPHABET[n].to_string();
+    }
+    let mut x = get_alpha((n / 26) - 1);
+    x.push(ALPHABET[n % 26]);
+    x
+}
 
 #[proc_macro_attribute]
 pub fn make_template(
@@ -9,9 +25,11 @@ pub fn make_template(
 ) -> proc_macro::TokenStream {
     let input_as_is = items.clone();
     let mut ast = parse_macro_input!(items as syn::DeriveInput);
+    let generics = &mut ast.generics.params;
 
     if let Data::Struct(ref mut x) = ast.data {
         let fields = &mut x.fields;
+        let mut ident_counter = 0;
         for field in fields {
             if let Type::Macro(ref mut x) = field.ty {
                 let macro_type = &x.mac;
@@ -25,9 +43,33 @@ pub fn make_template(
                     continue;
                 }
                 let tokens: TokenStream = macro_type.tokens.clone().into();
-                let parsed_types = parse_macro_input!(tokens as EitherMacro);
-                // handle multitype here
+                let parsed = parse_macro_input!(tokens as EitherMacro);
+                let mut new_generic_name = get_alpha(ident_counter);
+                while generics.iter().any(|x| {
+                    if let GenericParam::Type(x) = x {
+                        return x.ident.to_string() == new_generic_name;
+                    }
+                    false
+                }) {
+                    ident_counter += 1;
+                    new_generic_name = get_alpha(ident_counter);
+                }
+                let ident = Ident::new(&new_generic_name, Span::call_site());
+                generics.push(GenericParam::Type(syn::TypeParam {
+                    attrs: vec![],
+                    ident: ident.clone(),
+                    colon_token: None,
+                    bounds: Punctuated::new(),
+                    eq_token: None,
+                    default: None,
+                }));
+                field.ty = Type::Verbatim(ident.into_token_stream().into());
+
+                for parsed_type in parsed.types {
+                    // handle multitype here
+                }
             }
+            ident_counter += 1;
         }
     } else {
         return input_as_is;
@@ -37,20 +79,14 @@ pub fn make_template(
 }
 
 struct EitherMacro {
-    types: Vec<Type>,
+    types: Punctuated<Type, Token![|]>,
 }
 
 impl Parse for EitherMacro {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let mut out = vec![];
-        loop {
-            out.push(input.parse::<Type>()?);
-            if input.parse::<Token![|]>().is_err() {
-                break;
-            }
-        }
-
-        Ok(Self { types: out })
+        Ok(Self {
+            types: Punctuated::parse_separated_nonempty(input)?,
+        })
     }
 }
 
