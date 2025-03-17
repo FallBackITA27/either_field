bd2817a83659a873f1364c3458ea4308e0be3adb
Leila Massanova <49619073+FallBackITA27@users.noreply.github.com>
2025-03-17 12:25:25 +0100

    Get it Working (#7)
    Co-authored-by: ekstdo <minhala2003@mailo.com>

--- either_field/src/lib.rs
+++ either_field/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use quote::ToTokens;
+use quote::{ToTokens, quote};
 use syn::{
-    Data, GenericParam, Ident, Token, Type, parse::Parse, parse_macro_input, punctuated::Punctuated,
+    bracketed, parse::Parse, parse_macro_input, punctuated::Punctuated, GenericParam, Generics, Ident, Token, Type
 };
 
 fn get_alpha(n: usize) -> String {
@@ -20,73 +20,159 @@ fn get_alpha(n: usize) -> String {
 
 #[proc_macro_attribute]
 pub fn make_template(
-    _attr: proc_macro::TokenStream,
+    attr: proc_macro::TokenStream,
     items: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let input_as_is = items.clone();
-    let mut ast = parse_macro_input!(items as syn::DeriveInput);
-    let generics = &mut ast.generics.params;
+    let mut generic_struct = parse_macro_input!(items as syn::ItemStruct);
+    let initial_generics: Generics = generic_struct.generics.clone();
+    let generics = &mut generic_struct.generics.params;
 
-    if let Data::Struct(ref mut x) = ast.data {
-        let fields = &mut x.fields;
-        let mut ident_counter = 0;
-        for field in fields {
-            if let Type::Macro(ref mut x) = field.ty {
-                let macro_type = &x.mac;
-                if !macro_type
-                    .path
-                    .segments
-                    .iter()
-                    .any(|segment| segment.ident.to_string().as_str() == "either")
-                // TODO: need a better way to check whether the macro is the correct one
-                {
-                    continue;
-                }
-                let tokens: TokenStream = macro_type.tokens.clone().into();
-                let parsed = parse_macro_input!(tokens as EitherMacro);
-                let mut new_generic_name = get_alpha(ident_counter);
-                while generics.iter().any(|x| {
-                    if let GenericParam::Type(x) = x {
-                        return x.ident.to_string() == new_generic_name;
-                    }
-                    false
-                }) {
-                    ident_counter += 1;
-                    new_generic_name = get_alpha(ident_counter);
-                }
-                let ident = Ident::new(&new_generic_name, Span::call_site());
-                generics.push(GenericParam::Type(syn::TypeParam {
-                    attrs: vec![],
-                    ident: ident.clone(),
-                    colon_token: None,
-                    bounds: Punctuated::new(),
-                    eq_token: None,
-                    default: None,
-                }));
-                field.ty = Type::Verbatim(ident.into_token_stream().into());
+    // this also has to match the order of the generics
+    let mut ordered_idents_and_types: Vec<(Ident, Vec<Type>)> = vec![];
 
-                for parsed_type in parsed.types {
-                    // handle multitype here
-                }
+    let fields = &mut generic_struct.fields;
+    let mut ident_counter = 0;
+    for field in fields {
+        if let Type::Macro(ref mut x) = field.ty {
+            let macro_type = &x.mac;
+            if !macro_type
+                .path
+                .segments
+                .iter()
+                .any(|segment| segment.ident == "either")
+            // TODO: need a better way to check whether the macro is the correct one
+            {
+                continue;
             }
-            ident_counter += 1;
+
+            let tokens: TokenStream = macro_type.tokens.clone().into();
+            let parsed = parse_macro_input!(tokens as EitherMacro).0;
+
+            match field.clone().ident {
+                Some(ident) => ordered_idents_and_types.push((ident, parsed)),
+                None => continue,
+            };
+
+            let mut new_generic_name = get_alpha(ident_counter);
+            while generics.iter().any(|x| {
+                if let GenericParam::Type(x) = x {
+                    return x.ident == new_generic_name;
+                }
+                false
+            }) {
+                ident_counter += 1;
+                new_generic_name = get_alpha(ident_counter);
+            }
+            let ident = Ident::new(&new_generic_name, Span::call_site());
+            generics.push(GenericParam::Type(syn::TypeParam {
+                attrs: vec![],
+                ident: ident.clone(),
+                colon_token: None,
+                bounds: Punctuated::new(),
+                eq_token: None,
+                default: None,
+            }));
+            field.ty = Type::Verbatim(ident.into_token_stream());
         }
-    } else {
-        return input_as_is;
+        ident_counter += 1;
     }
 
-    ast.to_token_stream().into()
+    let derived_list = parse_macro_input!(attr as DerivedList).0;
+    let mut out = generic_struct.to_token_stream();
+    for derived in derived_list {
+        let types = ordered_idents_and_types
+            .iter()
+            .map(|(ident, possible_types)| match derived.fields.get(ident) {
+                None => possible_types[0].clone(),
+                Some(v) => match possible_types.contains(v) {
+                    false => {
+                        let error_message = format!("Type \"{}\" is not part of the specified possible types: {:?}",
+                            v.to_token_stream(),
+                            possible_types.iter().map(|x| format!("{}", x.to_token_stream())).collect::<Vec<_>>());
+                        out.extend::<proc_macro2::TokenStream>(quote! {  compile_error!(#error_message); });
+                        v.clone()
+                    },
+                    true => v.clone(),
+                },
+            });
+
+        let generic_name = generic_struct.ident.clone();
+        let generic_names: Vec<_> = initial_generics
+            .type_params()
+            .map(|x| x.ident.clone())
+            .collect();
+        let comma = if generic_names.is_empty() {
+            None
+        } else {
+            Some(syn::token::Comma::default())
+        };
+        let x = syn::ItemType {
+            type_token: syn::token::Type::default(),
+            semi_token: syn::token::Semi::default(),
+            eq_token: syn::token::Eq::default(),
+            attrs: vec![],
+            ident: derived.name,
+            vis: generic_struct.vis.clone(),
+            ty: std::boxed::Box::new(Type::Verbatim(quote! {
+                #generic_name<#(#generic_names),* #comma #(#types),*>
+            })),
+            generics: initial_generics.clone(),
+        };
+
+        out.extend::<proc_macro2::TokenStream>(x.into_token_stream());
+    }
+
+    out.into()
 }
 
-struct EitherMacro {
-    types: Punctuated<Type, Token![|]>,
+struct DerivedList(Vec<Derived>);
+impl Parse for DerivedList {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let parsed: Punctuated<Derived, Token![,]> = Punctuated::parse_terminated(input)?;
+        Ok(Self(parsed.into_iter().collect()))
+    }
 }
 
+struct Derived {
+    name: Ident,
+    fields: std::collections::HashMap<Ident, Type>,
+}
+impl Parse for Derived {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name = input.parse::<Ident>()?;
+        let _ = input.parse::<Token![:]>()?;
+        let field_list;
+        bracketed!(field_list in input);
+        let mut fields = std::collections::HashMap::new();
+        for field in
+            <Punctuated<FieldDescriptor, Token![,]>>::parse_separated_nonempty(&field_list)?
+        {
+            fields.insert(field.ident, field.field_type);
+        }
+
+        Ok(Self { name, fields })
+    }
+}
+
+struct FieldDescriptor {
+    ident: Ident,
+    field_type: Type,
+}
+impl Parse for FieldDescriptor {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident = input.parse::<Ident>()?;
+        let _ = input.parse::<Token![:]>()?;
+        let field_type = input.parse::<Type>()?;
+
+        Ok(Self { ident, field_type })
+    }
+}
+
+struct EitherMacro(Vec<Type>);
 impl Parse for EitherMacro {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        Ok(Self {
-            types: Punctuated::parse_separated_nonempty(input)?,
-        })
+        let parsed: Punctuated<Type, Token![|]> = Punctuated::parse_separated_nonempty(input)?;
+        Ok(Self(parsed.into_iter().collect()))
     }
 }
 
@@ -96,9 +182,9 @@ impl Parse for EitherMacro {
 // the syntax for make_template to be
 // syntactically valid according to the
 // compiler. This relies on the fact
-// that the attribute macro compiles
-// before the function-like macro
+// that the make_template macro compiles
+// before this function-like macro
 #[proc_macro]
-pub fn either(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    input
+pub fn either(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    TokenStream::new()
 }
